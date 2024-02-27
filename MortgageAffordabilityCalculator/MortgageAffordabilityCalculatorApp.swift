0cbac96efdede09b10ee6ce2598d9171eaf5f7c7
chavalk <chavalk@hotmail.com>
2024-02-27 14:55:15 -0600

    First commit.

---
+++ MortgageAffordabilityCalculator/MortgageAffordabilityCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MortgageAffordabilityCalculatorApp.swift
+//  MortgageAffordabilityCalculator
+//
+//  Created by Jose Garcia on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MortgageAffordabilityCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
