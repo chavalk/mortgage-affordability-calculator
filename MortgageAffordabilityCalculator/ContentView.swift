52581f6028a814ba73fdba37be5e3e7436870807
chavalk <chavalk@hotmail.com>
2024-02-29 19:54:00 -0600

    Add headline to app.

--- MortgageAffordabilityCalculator/ContentView.swift
+++ MortgageAffordabilityCalculator/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Mortgage Affordability Calculator")
+                .padding()
+                .font(.system(size: 25))
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
