15b4bdde3d16f638c4ec61963b8b70e06952d486
chavalk <chavalk@hotmail.com>
2024-02-29 20:12:18 -0600

    Add annual income text label and text field.

--- MortgageAffordabilityCalculator/ContentView.swift
+++ MortgageAffordabilityCalculator/ContentView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var annualIncome: String = ""
+    
     var body: some View {
         VStack {
             Text("Mortgage Affordability Calculator")
                 .padding()
                 .font(.system(size: 25))
+            
+            HStack {
+                Text("Annual Income")
+                
+                TextField("Enter Annual Income", text: $annualIncome)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .padding()
             
             Spacer()
         }
